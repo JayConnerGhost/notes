d9c769dcc7e8d23ff83cfea6c67879801b1c4d0f
trinity <ghosttk@protonmail.com>
2019-01-15 06:05:53 +0000

    Basic notepad functionality inc, new,save

---
+++ Notepad.UI/Notepad.UI/NotepadController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Notepad.UI
+{
+    public class NotepadController
+    {
+        private readonly NotepadFrame _notepadFrame;
+        private readonly Panel _notepadControlHolder;
+        private readonly MenuStrip _notepadMainMenu;
+        TextBox text;
+        public NotepadController(NotepadFrame notepadFrame)
+        {
+            _notepadFrame = notepadFrame;
+            _notepadControlHolder = notepadFrame.pnlControlHolder;
+            _notepadMainMenu = notepadFrame.menuMain;
+            CompositionStepTwo_addMenu();
+            CompositionStepOne_addText();
+
+        }
+
+        private void CompositionStepTwo_addMenu()
+        {
+            var mnuFile =new ToolStripMenuItem("file");
+            mnuFile.DropDownItems.Add(new ToolStripMenuItem("New",null,new EventHandler(New_Click)));
+            mnuFile.DropDownItems.Add(new ToolStripMenuItem("Save", null, new EventHandler(Save_Click)));
+            _notepadMainMenu.Items.Add(mnuFile);
+            _notepadMainMenu.Dock = DockStyle.Top;
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            var saveFileDialog1 = new SaveFileDialog {Filter = "txt|*.txt", Title = "Save a Text File"};
+            saveFileDialog1.ShowDialog();
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.FileStream fs = (System.IO.FileStream)saveFileDialog1.OpenFile();
+
+                string data = text.Text;
+                byte[] info = new UTF8Encoding(true).GetBytes(data);
+                fs.Write(info,0,info.Length);
+                byte[] btData = new byte[] { 0x0 };
+                fs.Write(btData, 0, btData.Length);
+                
+                fs.Close();
+                fs = null;
+            }
+        }
+
+        private void New_Click(object sender, EventArgs e)
+        {
+            text.Text = null;
+        }
+
+        private void CompositionStepOne_addText()
+        {
+           
+            text = new TextBox {Dock = DockStyle.Fill, Multiline = true};
+
+            _notepadControlHolder.Controls.Add(text);
+        }
+        
+    }
+}
---
+++ Notepad.UI/Notepad.UI/NotepadFrame.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Notepad.UI
+{
+    partial class NotepadFrame
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuMain = new System.Windows.Forms.MenuStrip();
+            this.pnlControlHolder = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // menuMain
+            // 
+            this.menuMain.Location = new System.Drawing.Point(0, 0);
+            this.menuMain.Name = "menuMain";
+            this.menuMain.Size = new System.Drawing.Size(806, 24);
+            this.menuMain.TabIndex = 0;
+            this.menuMain.Text = "menuStrip1";
+            // 
+            // pnlControlHolder
+            // 
+            this.pnlControlHolder.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlControlHolder.Location = new System.Drawing.Point(0, 24);
+            this.pnlControlHolder.Name = "pnlControlHolder";
+            this.pnlControlHolder.Size = new System.Drawing.Size(806, 431);
+            this.pnlControlHolder.TabIndex = 1;
+            // 
+            // NotepadFrame
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 455);
+            this.Controls.Add(this.pnlControlHolder);
+            this.Controls.Add(this.menuMain);
+            this.MainMenuStrip = this.menuMain;
+            this.Name = "NotepadFrame";
+            this.Text = "Notepad";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.MenuStrip menuMain;
+        public System.Windows.Forms.Panel pnlControlHolder;
+    }
+}
+
