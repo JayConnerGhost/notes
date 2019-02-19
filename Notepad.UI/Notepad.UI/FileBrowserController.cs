16fc4af5f9adfa39b65d3a21c36c096a5d46e585
j martin <ghosttk@protonmail.com>
2019-02-19 12:15:19 +0000

    adding exception handling

--- Notepad.UI/Notepad.UI/FileBrowserController.cs
+++ Notepad.UI/Notepad.UI/FileBrowserController.cs
@@ -23,8 +23,8 @@ namespace Notepad.UI
         private string GetStartingDirectory()
         {
             var startingDirectory = string.Empty;
-            var appSettingsReader =new System.Configuration.AppSettingsReader();
-            startingDirectory = (string)appSettingsReader.GetValue("startingFolder",typeof(string));
+            var appSettingsReader = new System.Configuration.AppSettingsReader();
+            startingDirectory = (string)appSettingsReader.GetValue("startingFolder", typeof(string));
             return startingDirectory;
         }
 
@@ -33,50 +33,56 @@ namespace Notepad.UI
             //work here to expand tree
             TreeNode newSelected = e.Node;
             _fileView.Items.Clear();
-            DirectoryInfo directory=(DirectoryInfo)newSelected.Tag;
+            DirectoryInfo directory = (DirectoryInfo)newSelected.Tag;
             ListViewItem.ListViewSubItem[] subItems;
             ListViewItem item = null;
-
-            foreach (var dir in directory.GetDirectories())
+            try
             {
-                item = new ListViewItem(dir.Name, 0);
-                subItems = new ListViewItem.ListViewSubItem[]
+
+
+                foreach (var dir in directory.GetDirectories())
                 {
+                    item = new ListViewItem(dir.Name, 0);
+                    subItems = new ListViewItem.ListViewSubItem[]
+                    {
                     new ListViewItem.ListViewSubItem(item, "Directory"),
                     new ListViewItem.ListViewSubItem(item, dir.LastAccessTime.ToShortDateString())
-                };
+                    };
 
-                item.SubItems.AddRange(subItems);
-                _fileView.Items.Add(item);
-            }
+                    item.SubItems.AddRange(subItems);
+                    _fileView.Items.Add(item);
+                }
 
-            foreach (FileInfo file in directory.GetFiles())
-            {
-                item=new ListViewItem(file.Name,1);
-                subItems=new ListViewItem.ListViewSubItem[]
+                foreach (FileInfo file in directory.GetFiles())
                 {
+                    item = new ListViewItem(file.Name, 1);
+                    subItems = new ListViewItem.ListViewSubItem[]
+                    {
                     new ListViewItem.ListViewSubItem(item,"File"),
                     new ListViewItem.ListViewSubItem(item,
                         file.LastAccessTime.ToShortDateString())
-                   
-                };
+                       };
 
-                item.SubItems.AddRange(subItems);
-                _fileView.Items.Add(item);
+                    item.SubItems.AddRange(subItems);
+                    _fileView.Items.Add(item);
 
+                }
             }
-
+            catch (System.UnauthorizedAccessException uae)
+            {
+                Console.WriteLine(uae.Message);
+            }
             _fileView.AutoResizeColumns(ColumnHeaderAutoResizeStyle.HeaderSize);
 
         }
 
-        private  void PopulateFolderView(string path)
+        private void PopulateFolderView(string path)
         {
             TreeNode rootNode;
-            DirectoryInfo info= new DirectoryInfo(path);
+            DirectoryInfo info = new DirectoryInfo(path);
             if (info.Exists)
             {
-                rootNode=new TreeNode(info.Name);
+                rootNode = new TreeNode(info.Name);
                 rootNode.Tag = info;
                 GetDirectories(info.GetDirectories(), rootNode);
                 _folderView.Nodes.Add(rootNode);
@@ -90,7 +96,7 @@ namespace Notepad.UI
             DirectoryInfo[] subSubDirs;
             foreach (DirectoryInfo subDir in getDirectories)
             {
-                aNode=new TreeNode(subDir.Name,0,0);
+                aNode = new TreeNode(subDir.Name, 0, 0);
                 aNode.Tag = subDir;
                 aNode.ImageKey = "folder";
                 try
@@ -105,9 +111,6 @@ namespace Notepad.UI
                 {
                     Console.WriteLine(uae.Message);
                 }
-               
-               
-
                 rootNode.Nodes.Add(aNode);
             }
         }
@@ -115,7 +118,6 @@ namespace Notepad.UI
         private void BuildFileBrowser()
         {
             this._container.Controls.Add(BuildOuterBrowser());
-
         }
 
         public SplitContainer BuildOuterBrowser()
@@ -124,22 +126,29 @@ namespace Notepad.UI
             {
                 Orientation = Orientation.Horizontal,
                 Dock = DockStyle.Fill,
-                SplitterDistance = 125,
+                SplitterDistance = 100,
                 SplitterWidth = 6
-                
+
             };
             outerContainer.Panel1.Name = "folderView";
             outerContainer.Panel2.Name = "fileView";
             _folderView = new TreeView { Dock = DockStyle.Fill };
-            _fileView =new ListView
+            _fileView = new ListView
             {
                 Dock = DockStyle.Fill,
                 GridLines = true,
                 View = View.List
             };
+
+            _fileView.ItemSelectionChanged += _fileView_ItemSelectionChanged;
             outerContainer.Panel1.Controls.Add(_folderView);
             outerContainer.Panel2.Controls.Add(_fileView);
             return outerContainer;
+        }
+
+        private void _fileView_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+
         }
 
         public void PopulateLocal(string path)
