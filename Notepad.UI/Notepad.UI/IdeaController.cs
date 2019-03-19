3c2cea07d49f2ef7af543e816f4ccb8ebeeab3bd
trinity <ghosttk@protonmail.com>
2019-03-19 10:25:07 +0000

    deleting ideas locally implemented , next step deleting them form the database

--- Notepad.UI/Notepad.UI/IdeaController.cs
+++ Notepad.UI/Notepad.UI/IdeaController.cs
@@ -6,6 +6,7 @@ namespace Notepad.UI
 {
     public class IdeaController
     {
+        private readonly TabControl _area;
         private IdeaService _ideaService;
 
       
@@ -20,10 +21,18 @@ namespace Notepad.UI
 
         public IdeaController(TabControl area, IdeaService ideaService) 
         {
+            _area = area;
             this._ideaService = ideaService;
-            BuildUIArea(area);
-            var ideaList = area.TabPages[1].Controls[0];
+            BuildUIArea(_area);
+            var ideaList = GetIdeaList(_area);
             PopulateData((ListView)ideaList);
+        }
+
+        private static Control GetIdeaList(TabControl area)
+        {
+            var table = area.TabPages[1].Controls[0];
+            var ideaList = table.Controls[0];
+            return ideaList;
         }
 
         private void BuildUIArea(TabControl area)
@@ -31,10 +40,37 @@ namespace Notepad.UI
             var itemList = new ListView {CheckBoxes = true, Dock = DockStyle.Fill};
             
             var tabPage = new TabPage {Dock = DockStyle.Fill,Text = "Ideas"};
-            tabPage.Controls.Add(itemList);
+            //add tablelayoutpanel
+            var tableLayout = new TableLayoutPanel {ColumnCount = 1, RowCount = 2,Dock = DockStyle.Fill};
+            tableLayout.Controls.Add(itemList,0,1);
+            var buttonLayout = new FlowLayoutPanel {FlowDirection = FlowDirection.LeftToRight,Height = 25,Dock = DockStyle.Top};
+            var addButton = new Button(){Text="Add"};
+            addButton.Click += AddButton_Click;
+            buttonLayout.Controls.Add(addButton);
+            var deleteButton = new Button(){Text = "Delete"};
+            deleteButton.Click += DeleteButton_Click;
+            buttonLayout.Controls.Add(deleteButton);
+            tableLayout.Controls.Add(buttonLayout,0,1);
+            
+            tabPage.Controls.Add(tableLayout);
             area.TabPages.Add(tabPage);
             itemList.View = View.List;
             
+        }
+
+        private void DeleteButton_Click(object sender, System.EventArgs e)
+        {
+            var selectedIdeas = ((ListView)GetIdeaList(_area)).CheckedItems;
+            //get selected controls 
+            foreach (ListViewItem idea in selectedIdeas)
+            {
+                idea.Remove();
+            }
+        }
+
+        private void AddButton_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("Add Record");
         }
     }
 }
