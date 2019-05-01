e3c543ba4abe08b63d92e033403954968bb9bfc5
jay katie martin <ghosttk@protonmail.com>
2019-05-01 21:16:22 +0100

    adding basic layout for webbrowser

--- Notepad.UI/Notepad.UI/BrowserController.cs
+++ Notepad.UI/Notepad.UI/BrowserController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Notepad.UI
 {
@@ -12,7 +13,13 @@ namespace Notepad.UI
         {
           var browserDialog=new DialogForm(new FormInfo("Browser",500, 500));
 
-          
+
+          var browserUrl = new TextBox {Dock = DockStyle.Top};
+          var toolBar=new ToolBar { Dock = DockStyle.Top };
+          var webBrowser = new WebBrowser {Dock = DockStyle.Fill};
+          browserDialog.Controls.Add(browserUrl);
+          browserDialog.Controls.Add(toolBar);
+          browserDialog.Controls.Add(webBrowser);
 
           browserDialog.ShowDialog();
         }
