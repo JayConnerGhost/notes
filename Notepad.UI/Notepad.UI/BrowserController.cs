3613f037226102780eac6c1d75db277a4a6a6d80
jay katie martin <ghosttk@protonmail.com>
2019-05-01 21:48:56 +0100

    Refactoring

--- Notepad.UI/Notepad.UI/BrowserController.cs
+++ Notepad.UI/Notepad.UI/BrowserController.cs
@@ -13,15 +13,28 @@ namespace Notepad.UI
         {
           var browserDialog=new DialogForm(new FormInfo("Browser",500, 500));
 
-
-          var browserUrl = new TextBox {Dock = DockStyle.Top};
-          var toolBar=new ToolBar { Dock = DockStyle.Top };
-          var webBrowser = new WebBrowser {Dock = DockStyle.Fill};
-          browserDialog.Controls.Add(browserUrl);
-          browserDialog.Controls.Add(toolBar);
-          browserDialog.Controls.Add(webBrowser);
-
+          ConstructUI(browserDialog);
           browserDialog.ShowDialog();
+        }
+
+        private void ConstructUI(DialogForm browserDialog)
+        {
+            var browserUrl = new TextBox { Dock = DockStyle.Top };
+            var toolBar = new ToolBar { Dock = DockStyle.Top };
+            BuildToolBar(toolBar);
+            var webBrowser = new WebBrowser { Dock = DockStyle.Fill };
+            browserDialog.Controls.Add(browserUrl);
+            browserDialog.Controls.Add(toolBar);
+            browserDialog.Controls.Add(webBrowser);
+        }
+
+        private void BuildToolBar(ToolBar toolBar)
+        {
+            //Add back 
+            //Add Forward
+            //Add Refresh
+
+            throw new NotImplementedException();
         }
     }
 }
